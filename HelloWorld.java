9d68eae454300cee3a7f5d049f92bdd229639ec2
a1ex-w] <alexwittman@live.com>
2024-09-05 18:38:26 -0700

    testing to make sure i sill remember how to comit lol

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
 public class HelloWorld {
     public static void main (String[] args) {
         String greeting = "Hello and welcome to my first program.";
-        System.out.print(greeting);
+        System.out.println(greeting);
+
+        System.out.println("I am testing pushing my code in this version :)");
     }
 }
